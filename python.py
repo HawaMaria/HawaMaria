895904b20b8962141c7625dad14ef280b7a6ec3c
HawaMaria <hawamaria33@gmail.com>
2022-10-05 17:18:50 +0300

    Basic plot

---
+++ python.py
@@ -0,0 +1,11 @@
+
+import matplotlib.pyplot as plt
+
+plt.plot([2000, 2001, 2002, 2003, 2004,2005], [2.0, 2.1, 2.2, 2.3, 2.4, 2.5])
+
+plt.ylabel('pop')
+plt.xlabel('year')
+
+plt.title("BASIC PLOT")
+
+plt.show()
